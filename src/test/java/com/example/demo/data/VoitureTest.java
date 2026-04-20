83d7dce43532d738c94556fb0ccf7d566676abd5
EDOH-DAGNON Clarence <clarenceedohdagnon@gmail.com>
2026-04-20 10:18:26 +0200

    modification de la classe VoitureTest.java

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,9 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class VoitureTest {
 
+    @BeforeEach
+    void initVoitureTest(){
+        String marque = "Renault";
+        int prix = 1000;
+        Voiture voiture = new Voiture(marque,prix);
+    }
+
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        assertEquals(marque, voiture.getMarque);
+        assertEquals(prix, voiture.getPrix);
+    }
+
+    @Test
+    void modifVoiture(){
+        String newMarque = "Peugeot";
+        int newPrix = 2000;
+        voiture.setMarque(newMarque);
+        voiture.setPrix(newPrix);
+        voiture.setId(10);
+        assertEquals(newMarque, voiture.getMarque);
+        assertEquals(prix, voiture.getPrix);
+        assertEquals(10, voiture.getId);
     }
 
 }
