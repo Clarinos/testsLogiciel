fcdc6b04b8b9fa3b0054b30e513183bc95167322
EDOH-DAGNON Clarence <12402696@r20320.iutv.univ-paris13.fr>
2026-05-20 16:24:03 +0200

    init StatistiqueTests

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -4,6 +4,10 @@ import com.example.demo.data.Voiture;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.mockito.Mockito.*;
 
@@ -11,7 +15,36 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    private static StatistiqueImpl stat;
 
+    @BeforeEach
+    public void init(){
+        StatistiqueTests.stat = new StatistiqueImpl();
+        Voiture voiture1 = new Voiture("Renault", 1000);
+        Voiture voiture2 = new Voiture("Peugeot", 4000);
+        StatistiqueTests.stat.ajouter(voiture1);
+        StatistiqueTests.stat.ajouter(voiture2);
+        Echantillon echantillon = StatistiqueTests.stat.getPrixMoyen();
+    }
+
+    @Test
+    public void modifStatistique(){
+        assertEquals(echantillon.getNombreDeVoitures(),2);
+        assertEquals(echantillon.prixMoyen().getPrixMoyen(),2500);
+
+    }
+
+    @Test
+    public void modifEchantillonRecu(){
+        echantillon.setNombreDeVoitures(40);
+        echantillon.setPrixMoyen(6500);
+        assertEquals(echantillon.getPrixMoyen(),6500);
+        assertEquals(echantillon.getNombreDeVoitures,40);
+
+    }
+
+
+
+    
+        
 }
