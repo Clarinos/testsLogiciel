12de55f254e05e5fb5d1561d17c1d8bd330df8b2
Clarence <clarenceedohdagnon@gmail.com>
2026-06-17 14:23:13 +0200

    Revert "Tests StatistiqueController"
    This reverts commit 918b4d2f3beaa292a9d04b0adbad4108757cf65a.

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -6,9 +6,9 @@ import com.example.demo.service.StatistiqueImpl;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,26 +18,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+// @AutoConfigureMockMvc
 class WebTests {
 
-    @MockBean
+    // @MockBean
     StatistiqueImpl statistiqueImpl;
 
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    public void getStatistiques() throws Exception {
-        
-        doNothing().when(statistiqueImpl).ajouter(new Voiture("Renault", 3000));
-        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 3000));
-        
-        mockMvc.perform(get("/statistique"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nombreDeVoitures").value("1"))
-                .andExpect(jsonPath("$.prixMoyen").value("5000"))
-                .andReturn();
-    }
 }
